107201a2a2ce44ec722a91f89c3f34d5392b8014
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2024-11-11 17:20:21 +0300

    -

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 login = os.getenv('login')
 password = os.getenv('password')
+my_mail = 'nazaryan_suren@bk.ru'
+friend_mail = 'suren-nazaryan228@mail.ru'
 
-letter = f"""From: nazaryan_suren@bk.ru
-To: suren-nazaryan_228@mail.ru
+letter = f"""From: %my_mail%
+To: %friend_mail%
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";
 
@@ -28,33 +30,12 @@ Content-Type: text/plain; charset="UTF-8";
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".replace('%website%','https://dvmn.org/profession-ref-program/skyephirts/uzo2M/').replace('%friend_name%', 'Сурен').replace('%my_name%', 'Сурен')
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".replace('%website%','https://dvmn.org/profession-ref-program/skyephirts/uzo2M/').replace('%friend_name%', 'Сурен').replace('%my_name%', 'Сурен').replace('%my_mail%', 'nazaryan_suren@bk.ru').replace('%friend_mail%', 'suren-nazaryan_228@mail.ru')
 
 letter = letter.encode("UTF-8")
 
 server = smtplib.SMTP_SSL('smtp.mail.ru:465')
 server.login(login, password)
-server.sendmail('nazaryan_suren@bk.ru','suren-nazaryan228@mail.ru', letter)
+server.sendmail(my_mail, friend_mail, letter)
 server.quit()
-
-website = 'https://dvmn.org/profession-ref-program/skyephirts/uzo2M/'
-friend_name = 'Сурен'
-my_name = 'Сурен'
-new_letter = f'''Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
-
-{website} — это новая версия онлайн-курса по программированию. 
-Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
-
-Как будет проходить ваше обучение на {website}? 
-
-→ Попрактикуешься на реальных кейсах. 
-Задачи от тимлидов со стажем от 10 лет в программировании.
-→ Будешь учиться без стресса и бессонных ночей. 
-Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
-→ Подготовишь крепкое резюме.
-Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
-
-Регистрируйся → {website} 
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''
-
 
